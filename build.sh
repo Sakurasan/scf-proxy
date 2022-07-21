c19ecb730256b533fe791038b65c96a74b300d67
y1n <amzza@protonmail.com>
2022-07-21 15:29:06 +0800

    更新：构建脚本更新

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 work_path=$(cd `dirname $0`/../; pwd)
 echo $work_path
 
-GOOS=linux GOARCH=amd64 go build -o main cmd/main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -v -a -ldflags '-s -w' -gcflags="all=-trimpath=${PWD}" -asmflags="all=-trimpath=${PWD}" -o main cmd/main.go
 zip main.zip main
